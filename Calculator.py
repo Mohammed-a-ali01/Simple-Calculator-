8614ecf7b21932100e785fc3b27484d59f567672
Mohammed Ali <Mohammed.a_ali@outlook.com>
2021-06-01 18:48:10 -0400

    calculator

--- Calculator.py
+++ Calculator.py
@@ -22,4 +22,5 @@ while operator !="xyz":
             print("{} / {}".format(num1,num2))
             print(num1/num2)
         else:
-            print("You have not provided an accurate operator, please try again")
+
+            print("You have not provided an accurate operator, please try again\n")
